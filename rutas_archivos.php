0af96742d13b4075195c23c1d7db0f0dfabc84ad
Alexander Moreno <alexander.morenoc@outlook.com>
2020-05-20 01:47:47 -0500

    quite Partes de las rutas

--- rutas_archivos.php
+++ rutas_archivos.php
@@ -4,21 +4,21 @@
 //VALIDO PARA UTILIZAR EN LOS HREF Y NO TANTO EN INCLUDE CON RUTAS ABSOLUTAS
     
 
-    define('INDEX', "/Partes/Inicio/index.php");
+    define('INDEX', "/Inicio/index.php");
 
-    define('TRADUCTOR', '/Partes/Traductor/traductor.php');
+    define('TRADUCTOR', '/Traductor/traductor.php');
 
-    define('PLATAFORMA_APRENDIZAJE', '/Partes/plataforma/plataforma_aprendizaje.php');
+    define('PLATAFORMA_APRENDIZAJE', '/plataforma/plataforma_aprendizaje.php');
 
-    define('CONTACTANOS', "/Partes/contactanos/contactos.php");
+    define('CONTACTANOS', "/contactanos/contactos.php");
 
 
 
-    define('MY_PERFIL', "/Partes/perfiles/usuario.php");
+    define('MY_PERFIL', "/perfiles/usuario.php");
 
-    define('LOGEO','/Partes/logeo/login.php');
+    define('LOGEO','/logeo/login.php');
 
-    define('LOGOUT', '/Partes/logeo/logout.php');
+    define('LOGOUT', '/logeo/logout.php');
     
     define('FOOTER', "../footer.html");
 
@@ -27,27 +27,27 @@
 
 //ESTILO E ICONOS y Jquery
 
-    define('ESTILO_GENERAL', "/Partes/assets/css/estilos.css");
-    define('ESTILO_RESPONSIVE', "/Partes/assets/css/estilos_responsive.css");
+    define('ESTILO_GENERAL', "/assets/css/estilos.css");
+    define('ESTILO_RESPONSIVE', "/assets/css/estilos_responsive.css");
 
-    define('ESTILO_ARTICULOS', "/Partes/assets/css/estilos_articulos.css");
+    define('ESTILO_ARTICULOS', "/assets/css/estilos_articulos.css");
 
-    define('ESTILO_LOGIN',"/Partes/assets/css/estilos_login.css");
+    define('ESTILO_LOGIN',"/assets/css/estilos_login.css");
 
-    define('ESTILO_TRADUCTOR','/Partes/assets/css/estilos_traductor.css');
+    define('ESTILO_TRADUCTOR','/assets/css/estilos_traductor.css');
 
-    define('ICONOS1', "/Partes/assets/css/font.css");
-    define('ICONOS2', "/Partes/assets/css_icon/css/all.min.css");
+    define('ICONOS1', "/assets/css/font.css");
+    define('ICONOS2', "/assets/css_icon/css/all.min.css");
 
-    define('JQUERY', "/Partes/assets/js/jquery-3.4.1.js");
+    define('JQUERY', "/assets/js/jquery-3.4.1.js");
 
 
 
 //ARTICULOS estos si estan dentro de esta direccion tener cuidado
-    define('ART1', "/Partes/Inicio/vista/articulos/art_biolouis.php");
-    define('ART2', "/Partes/Inicio/vista/articulos/art_sistemabraille.php");
-    define('ART3', "/Partes/Inicio/vista/articulos/art_herramientas.php");
-    define('ART4', "/Partes/Inicio/vista/articulos/art_4.php");
+    define('ART1', "/Inicio/vista/articulos/art_biolouis.php");
+    define('ART2', "/Inicio/vista/articulos/art_sistemabraille.php");
+    define('ART3', "/Inicio/vista/articulos/art_herramientas.php");
+    define('ART4', "/Inicio/vista/articulos/art_4.php");
 
 
 //libreria artyom
