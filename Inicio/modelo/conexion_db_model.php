c5e0f470dcea8ed4ec8d30f76228921a6c813c3a
Alexander Moreno <alexander.morenoc@outlook.com>
2020-05-20 01:07:21 -0500

    Preparando datos bd para subir a la web se hizo merge de lexdev

--- Inicio/modelo/conexion_db_model.php
+++ Inicio/modelo/conexion_db_model.php
@@ -1,16 +1,18 @@
-<?php
 
-    $server='localhost';
-    $username='root';
-    $password='';
-    $database='php_login_database';
+
+ <?php
 
     $server2='localhost';
-    $username2='id12340850_alexander';
-    $password2='alexander9';
-    $database2='id12340850_webbraillebd';
-    
+    $username2='root';
+    $password2='';
+    $database2='php_login_database';
 
+    $server='localhost';
+    $username='lexsuqwu_adminwb';
+    $password='413*4ndeR9';
+    $database='lexsuqwu_webbraille';
+    
+//contraseña de usuario en wp:413*4ndeR9
     try{
         $conn=new PDO("mysql:host=$server; dbname=$database;",$username,$password);
     }catch(PDOException $e){
